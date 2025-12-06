7478300ef7db0b2be55ec53802860f2daca1a9bc
Roman Havryliuk <romanfest1221@gmail.com>
2025-12-06 17:46:15 +0100

    add task 4-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    const cleanEmail = email.trim();
+    const cleanPassword = password.trim();
+    if (cleanEmail === "" || cleanPassword === "") {
+        alert('All form fields must be filled in');
+        return;
+    };
+    const data = {
+        email: cleanEmail,
+        password: cleanPassword,
+    };
+    console.log(data);
+    form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const clickMe = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+const spanColor = document.querySelector('.color')
+
+clickMe.addEventListener('click', handleBtn);
+
+function handleBtn(event) {
+  const color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  spanColor.textContent = color;
+}
